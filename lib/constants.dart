283c788cc0f802f9d74b8653dc4afd8be210e47f
RamanYarotski <rvlogist@gmail.com>
2022-02-25 16:41:43 +0300

    [ADD] - Init;

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+//Colors
+const primaryColor = orange;
+const secondaryColor = orange2;
+const backgroundColor = gray84;
+const titleTextColor = black1;
+const subtitleTextColor = gray70;
+
+//Gray palette
+const black1 = Color(0xFF000000);
+const gray15 = Color(0xFF262626);
+const gray30 = Color(0xFF4D4D4D);
+const gray70 = Color(0xFFB2B2B2);
+const gray80 = Color(0xFFCCCCCC);
+const gray84 = Color (0xFFD7D7D7);
+const gray100 = Color(0xFCFCFCFC);
+//Orange palette
+const orange = Color(0xFFF27434);
+const orange2 = Color(0xFFBE5E34);
