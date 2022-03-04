1e848668d7353d271e3e949fb420a537cde385d2
Raman <rvlogist@gmail.com>
2022-03-04 20:16:42 +0300

    refactor: change structure of the project and add pictures

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:training_project/constants.dart';
-import 'package:training_project/models/user.dart';
-import '../components/widgets.dart';
+import 'package:training_project/widgets/export.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -29,74 +29,10 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
             Expanded(
               child: Container(
                 padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: TabBarView(controller: tabController, children: [
+                child: TabBarView(controller: tabController, children: const [
+                  FeedBar(),
                   Center(
-                    child: Column(children: [
-                      ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: users.length,
-                        itemBuilder: (context, int index) {
-                          final currentUser = users[index];
-                          return Card(
-                              margin: const EdgeInsets.only(top: 16),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Column(
-                                      children: [
-                                        _circleAvatar(currentUser.avatarSource),
-                                        const SizedBox(
-                                          height: 8,
-                                        ),
-                                        Text(
-                                          currentUser.userName,
-                                          style: userNameTextStyle,
-                                        )
-                                      ],
-                                    ),
-                                    const SizedBox(
-                                      width: 16,
-                                    ),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        _itemTextRow(
-                                            currentUser,
-                                            itemTitleTextStyleBold,
-                                            itemTitleTextStyle),
-                                        const SizedBox(
-                                          height: 8,
-                                        ),
-                                        _itemTextRow(
-                                            currentUser,
-                                            itemSubTitleTextStyleBold,
-                                            itemSubTitleTextStyle),
-                                      ],
-                                    ),
-                                    const Spacer(),
-                                    IconButton(
-                                        iconSize: 22,
-                                        alignment: Alignment.topRight,
-                                        padding: EdgeInsets.zero,
-                                        onPressed: () {},
-                                        icon: const Icon(
-                                          Icons.more_vert,
-                                          color: gray70,
-                                        )),
-                                  ],
-                                ),
-                              ));
-                        },
-                      )
-                    ]),
-                  ),
-                  const Center(
-                    child: Text("NOTIFICATIONS page"),
+                    child: Text( "NOTIFICATIONS page"),
                   )
                 ]),
               ),
@@ -105,77 +41,6 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
         ));
   }
 }
-
-TextStyle userNameTextStyle =
-    const TextStyle(fontSize: 8, color: subtitleTextColor);
-
-TextStyle itemTitleTextStyle = const TextStyle(fontSize: 18);
-TextStyle itemTitleTextStyleBold =
-    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
-
-TextStyle itemSubTitleTextStyle =
-    const TextStyle(fontSize: 12, color: subtitleTextColor);
-TextStyle itemSubTitleTextStyleBold = const TextStyle(
-    fontSize: 12, color: subtitleTextColor, fontWeight: FontWeight.bold);
-
-Row _itemTextRow(
-    User currentUser, TextStyle itemTextStyle, TextStyle itemSecondTextStyle) {
-  return Row(
-    children: [
-      Text(currentUser.number.toString(), style: itemTextStyle),
-      const SizedBox(
-        width: 4,
-      ),
-      Text(currentUser.numberValue.toString(), style: itemTextStyle),
-      const SizedBox(
-        width: 4,
-      ),
-      Text(currentUser.time.toString(), style: itemSecondTextStyle),
-    ],
-  );
-}
-
-Widget _circleAvatar(String imageRoute) {
-  return CircleAvatar(
-    backgroundImage: AssetImage(imageRoute),
-    radius: avatarSize,
-  );
-}
-
-// Widget _circleAvatar2(String imageRoute) {
-//   return Container(
-//     decoration: const BoxDecoration(
-//       shape: BoxShape.circle,
-//     ),
-//     margin: const EdgeInsets.all(4),
-//     child: Image.asset(
-//       imageRoute,
-//       width: 80,
-//       fit: BoxFit.cover,
-//     ),
-//   );
-// }
-
-// Widget _circleAvatar3(String imageRoute) {
-//   return Container(
-//     decoration: BoxDecoration(
-//       image: DecorationImage(image: AssetImage(imageRoute), fit: BoxFit.fill),
-//       color: Colors.white,
-//       shape: BoxShape.circle,
-//     ),
-//   );
-// }
-
-// Widget _circleAvatar4(String imageRoute) {
-//   return CircleAvatar(
-//       backgroundColor: Colors.transparent,
-//       child: SizedBox(
-//           width: 60,
-//           height: 60,
-//           child: ClipOval(
-//             child: Image.asset(imageRoute),
-//           )));
-// }
 
 AppBar buildAppBar() {
   return AppBar(
---
+++ lib/widgets/export.dart
@@ -0,0 +1,2 @@
+export 'package:training_project/widgets/HomeScreen/feed_bar.dart';
+export 'package:training_project/widgets/HomeScreen/my_tab_bar.dart';
