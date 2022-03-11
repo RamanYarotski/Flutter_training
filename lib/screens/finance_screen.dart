3934de4b0e871724ee214e569db9ff293afebd9d
Raman <rvlogist@gmail.com>
2022-03-11 18:01:18 +0300

    refactor: extract widgets

--- lib/screens/finance_screen.dart
+++ lib/screens/finance_screen.dart
@@ -34,83 +34,117 @@ class FinanceScreen extends StatelessWidget {
         body: Stack(
           alignment: Alignment.topCenter,
           children: [
-            Container(
-                height: headerHeight,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets/images/background.png"),
-                        fit: BoxFit.cover)),
-                child: SafeArea(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: headerHeight * appBarTitleHeightCoeff,
-                      ),
-                      Text(Strings.finance,
-                          style: const TextStyle(
-                              fontSize: subtitleSize,
-                              color: gray100,
-                              fontWeight: FontWeight.w300,
-                              fontFamily: "DMSans")),
-                      SizedBox(
-                        height: headerHeight * titleHeightCoeff,
-                      ),
-                      Text(user.balance.toStringDollarPattern(),
-                          style: const TextStyle(
-                              fontSize: titleSize,
-                              color: gray100,
-                              fontWeight: FontWeight.w500,
-                              fontFamily: "DMSans")),
-                      Text(Strings.currentBalance,
-                          style: const TextStyle(
-                              fontSize: subtitleSize,
-                              color: gray100,
-                              fontWeight: FontWeight.w300,
-                              fontFamily: "DMSans")),
-                    ],
-                  ),
-                )),
-            Column(
-              children: [
-                SizedBox(
-                  height: size.height - bodyHeight,
-                ),
-                SizedBox(
-                    width: size.width,
-                    height: bodyHeight,
-                    child: ListView(
-                        padding: const EdgeInsets.all(0),
-                        children: <Widget>[
-                          FinanceCard(
-                              bodyHeight: bodyHeight,
-                              cardName: Strings.expenses,
-                              currentParameterValue: user.expensesActual,
-                              plannedParameterValue: user.expensesPlanned,
-                              currentParameterName: Strings.actual,
-                              plannedParameterName: Strings.planned),
-                          FinanceCard(
-                            bodyHeight: bodyHeight,
-                            cardName: Strings.income,
-                            currentParameterValue: user.incomeActual,
-                            plannedParameterValue: user.incomeMonthly,
-                            currentParameterName: Strings.actual,
-                            plannedParameterName: Strings.monthlyIncome,
-                          ),
-                          FinanceCard(
-                            bodyHeight: bodyHeight,
-                            cardName: Strings.goal,
-                            currentParameterValue: user.goalSave,
-                            plannedParameterValue: user.goalTotalAmount,
-                            currentParameterName: Strings.save,
-                            plannedParameterName: Strings.totalAmount,
-                          )
-                        ])),
-              ],
-            ),
+            Header(headerHeight: headerHeight, user: user),
+            Body(size: size, bodyHeight: bodyHeight, user: user),
           ],
         ),
       ),
     );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({
+    Key key,
+    @required this.size,
+    @required this.bodyHeight,
+    @required this.user,
+  }) : super(key: key);
+
+  final Size size;
+  final double bodyHeight;
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: size.height - bodyHeight,
+        ),
+        SizedBox(
+            width: size.width,
+            height: bodyHeight,
+            child: ListView(
+                padding: const EdgeInsets.all(0),
+                children: <Widget>[
+                  FinanceCard(
+                      bodyHeight: bodyHeight,
+                      cardName: Strings.expenses,
+                      currentParameterValue: user.expensesActual,
+                      plannedParameterValue: user.expensesPlanned,
+                      currentParameterName: Strings.actual,
+                      plannedParameterName: Strings.planned),
+                  FinanceCard(
+                    bodyHeight: bodyHeight,
+                    cardName: Strings.income,
+                    currentParameterValue: user.incomeActual,
+                    plannedParameterValue: user.incomeMonthly,
+                    currentParameterName: Strings.actual,
+                    plannedParameterName: Strings.monthlyIncome,
+                  ),
+                  FinanceCard(
+                    bodyHeight: bodyHeight,
+                    cardName: Strings.goal,
+                    currentParameterValue: user.goalSave,
+                    plannedParameterValue: user.goalTotalAmount,
+                    currentParameterName: Strings.save,
+                    plannedParameterName: Strings.totalAmount,
+                  )
+                ])),
+      ],
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({
+    Key key,
+    @required this.headerHeight,
+    @required this.user,
+  }) : super(key: key);
+
+  final double headerHeight;
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: headerHeight,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/background.png"),
+                fit: BoxFit.cover)),
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                height: headerHeight * appBarTitleHeightCoeff,
+              ),
+              Text(Strings.finance,
+                  style: const TextStyle(
+                      fontSize: subtitleSize,
+                      color: gray100,
+                      fontWeight: FontWeight.w300,
+                      fontFamily: "DMSans")),
+              SizedBox(
+                height: headerHeight * titleHeightCoeff,
+              ),
+              Text(user.balance.toStringDollarPattern(),
+                  style: const TextStyle(
+                      fontSize: titleSize,
+                      color: gray100,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: "DMSans")),
+              Text(Strings.currentBalance,
+                  style: const TextStyle(
+                      fontSize: subtitleSize,
+                      color: gray100,
+                      fontWeight: FontWeight.w300,
+                      fontFamily: "DMSans")),
+            ],
+          ),
+        ));
   }
 }
--- lib/widgets/FinanceScreen/finance_card.dart
+++ lib/widgets/FinanceScreen/finance_card.dart
@@ -45,123 +45,172 @@ class FinanceCard extends StatelessWidget {
       shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(6))),
       child: Container(
-        padding: const EdgeInsets.fromLTRB(financeItemPadding,
-            financeItemPadding, financeItemPadding * half, financeItemPadding*1.5),
+        padding: const EdgeInsets.fromLTRB(
+            financeItemPadding,
+            financeItemPadding,
+            financeItemPadding * half,
+            financeItemPadding * 1.5),
         height: bodyHeight / financeItemsCount - financeItemMargin * 2,
         child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    cardName,
-                    style: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.w600),
-                  ),
-                   IconButton(
-                    padding: EdgeInsets.zero,
-                    splashRadius: 20,
-                    iconSize: 26,
-                    onPressed: () {},
-                    icon: const  Icon(
-                      Icons.keyboard_arrow_right_rounded,
-                      // size: 26,
-                      color: gray70,
-                    ),
-                  ),
-                ],
-              ),
-              Row(children: [
-                Expanded(
-                  flex: 8,
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            currentParameterValue.toStringDollarPattern(),
-                            style: const TextStyle(
-                                fontSize: 22, fontWeight: FontWeight.w600),
-                          ),
-                          Text(
-                            plannedParameterValue.toStringDollarPattern(),
-                            style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
-                                color: gray50),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 4,
-                      ),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              currentParameterName,
-                              style: financeItemSubtitleTextStyle,
-                            ),
-                            Text(plannedParameterName,
-                                style: financeItemSubtitleTextStyle),
-                          ])
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
-                  child: Container(
-                    height: buttonsSize,
-                    width: buttonsSize,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: lilac,
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 3,
-                              blurRadius: 5,
-                              offset: const Offset(0, 2)),
-                        ]),
-                    child: const Icon(
-                      Icons.add,
-                      color: gray100,
-                    ),
-                  ),
-                ),
-              ]),
-              Row(
-                children: [
-                  LinearPercentIndicator(
-                    padding: const EdgeInsets.all(0),
-                    lineHeight: indicatorHeight,
-                    backgroundColor: gray84,
-                    animation: true,
-                    linearGradient:
-                        const LinearGradient(colors: <Color>[lilac, blue]),
-                    curve: Curves.linear,
-                    animateFromLastPercent: true,
-                    animationDuration: 1500,
-                    center: Text(
-                      "${progressPercent.toString()}%",
-                      style: const TextStyle(
-                          color: black1, fontSize: indicatorHeight),
-                    ),
-                    width: MediaQuery.of(context).size.width -
-                        financeItemPadding * 2 -
-                        financeItemMargin * 2 -
-                        5,
-                    barRadius: const Radius.circular(indicatorHeight * half),
-                    percent: proportionOfProgress,
-                  ),
-                  // ),
-                ],
-              )
+              TitleRow(cardName: cardName),
+              ValueRow(
+                  currentParameterValue: currentParameterValue,
+                  plannedParameterValue: plannedParameterValue,
+                  currentParameterName: currentParameterName,
+                  plannedParameterName: plannedParameterName),
+              ProgressRow(
+                  progressPercent: progressPercent,
+                  proportionOfProgress: proportionOfProgress)
             ]),
       ),
+    );
+  }
+}
+
+class ProgressRow extends StatelessWidget {
+  const ProgressRow({
+    Key key,
+    @required this.progressPercent,
+    @required this.proportionOfProgress,
+  }) : super(key: key);
+
+  final int progressPercent;
+  final double proportionOfProgress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        LinearPercentIndicator(
+          padding: const EdgeInsets.all(0),
+          lineHeight: indicatorHeight,
+          backgroundColor: gray84,
+          animation: true,
+          linearGradient: const LinearGradient(colors: <Color>[lilac, blue]),
+          curve: Curves.linear,
+          animateFromLastPercent: true,
+          animationDuration: 1500,
+          center: Text(
+            "${progressPercent.toString()}%",
+            style: const TextStyle(color: black1, fontSize: indicatorHeight),
+          ),
+          width: MediaQuery.of(context).size.width -
+              financeItemPadding * 2 -
+              financeItemMargin * 2 -
+              5,
+          barRadius: const Radius.circular(indicatorHeight * half),
+          percent: proportionOfProgress,
+        ),
+        // ),
+      ],
+    );
+  }
+}
+
+class ValueRow extends StatelessWidget {
+  const ValueRow({
+    Key key,
+    @required this.currentParameterValue,
+    @required this.plannedParameterValue,
+    @required this.currentParameterName,
+    @required this.plannedParameterName,
+  }) : super(key: key);
+
+  final int currentParameterValue;
+  final int plannedParameterValue;
+  final String currentParameterName;
+  final String plannedParameterName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Expanded(
+        flex: 8,
+        child: Column(
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  currentParameterValue.toStringDollarPattern(),
+                  style: const TextStyle(
+                      fontSize: 22, fontWeight: FontWeight.w600),
+                ),
+                Text(
+                  plannedParameterValue.toStringDollarPattern(),
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w600, color: gray50),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Text(
+                currentParameterName,
+                style: financeItemSubtitleTextStyle,
+              ),
+              Text(plannedParameterName, style: financeItemSubtitleTextStyle),
+            ])
+          ],
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(left: 20.0),
+        child: Container(
+          height: buttonsSize,
+          width: buttonsSize,
+          decoration:
+              BoxDecoration(shape: BoxShape.circle, color: lilac, boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 3,
+                blurRadius: 5,
+                offset: const Offset(0, 2)),
+          ]),
+          child: const Icon(
+            Icons.add,
+            color: gray100,
+          ),
+        ),
+      ),
+    ]);
+  }
+}
+
+class TitleRow extends StatelessWidget {
+  const TitleRow({
+    Key key,
+    @required this.cardName,
+  }) : super(key: key);
+
+  final String cardName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          cardName,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+        ),
+        IconButton(
+          padding: EdgeInsets.zero,
+          splashRadius: 20,
+          iconSize: 26,
+          onPressed: () {},
+          icon: const Icon(
+            Icons.keyboard_arrow_right_rounded,
+            // size: 26,
+            color: gray70,
+          ),
+        ),
+      ],
     );
   }
 }
