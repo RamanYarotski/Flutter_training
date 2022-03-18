0f138ff9187b27a7db14fe564ed6b194bf4f5855
Raman <rvlogist@gmail.com>
2022-03-18 10:38:33 +0300

    fix: fix error when using BlocProvider.of

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Flutter Demo",
-      theme: lightThemeData(context),
-      darkTheme: darkThemeData(context),
-      themeMode: ThemeMode.system,
-      home:  BlocProvider(
-        create: (context) => SelectFinanceCardBlock(),
-        child: const HomeScreen(),
+    return BlocProvider(
+      create: (context) => SelectFinanceCardBlock(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Flutter Demo",
+        theme: lightThemeData(context),
+        darkTheme: darkThemeData(context),
+        themeMode: ThemeMode.system,
+        home: const HomeScreen(),
       ),
     );
   }
