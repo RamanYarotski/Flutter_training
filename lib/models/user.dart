2625cd3ba3be6d8e26cee2b9c42e40c04b87c576
Raman <rvlogist@gmail.com>
2022-03-18 15:55:38 +0300

    feat: add data layer

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:training_project/constants.dart';
+
 class User {
   String avatarSource;
   String userName;
@@ -12,7 +16,7 @@ class User {
   int goalSave;
   int goalTotalAmount;
   User(
-      this.avatarSource,
+      {this.avatarSource,
       this.userName,
       this.number,
       this.numberValue,
@@ -23,22 +27,55 @@ class User {
       this.incomeActual,
       this.incomeMonthly,
       this.goalSave,
-      this.goalTotalAmount);
+      this.goalTotalAmount});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    var rnd = Random();
+
+    return User(
+      avatarSource: 'assets/images/1.jpeg","Henry Graves',
+      userName: json['userName'],
+      number: json['id'],
+      numberValue: '№ ID',
+      time: json['name'],
+      balance: rnd.nextInt(randomMaxDigit),
+      expensesActual: rnd.nextInt(randomMaxDigit),
+      expensesPlanned: rnd.nextInt(randomMaxDigit),
+      incomeActual: rnd.nextInt(randomMaxDigit),
+      incomeMonthly: rnd.nextInt(randomMaxDigit),
+      goalSave: rnd.nextInt(randomMaxDigit),
+      goalTotalAmount: rnd.nextInt(randomMaxDigit),
+    );
+  }
 }
 
-final List<User> users = <User>[
-  User("assets/images/1.jpeg","Henry Graves",1,"Day","12:25:14:355",24847, 97, 1429, 1751,3836, 0, 35962  ),
-  User("assets/images/2.jpeg", "Johni Holmes", 1, "Day", "12:25:14:355",4847, 72, 1429, 1751,3836, 10, 39962  ),
-  User("assets/images/3.jpeg", "Alma Simens", 1, "Day", "12:25:14:355",14847, 972, 1429, 1751,3836, 110, 25962  ),
-  User("assets/images/4.jpg", "Henry Graves", 1, "Day", "12:25:14:355",24667, 7, 1429, 1751,3836, 40, 115962  ),
-  User("assets/images/5.jpg", "Johni Holmes", 1, "Day", "12:25:14:355",847, 9, 1429, 1751,3836, 540, 55962  ),
-  User("assets/images/6.jpg", "Alma Simens", 1, "Day", "12:25:14:355",5847, 92, 1429, 1751,3836, 40, 65962  ),
-  User("assets/images/7.jpg", "Henry Graves", 1, "Day", "12:25:14:355",2847, 2, 1429, 1751,3836, 100, 15962  ),
-  User("assets/images/1.jpeg", "Henry Graves", 1, "Day", "12:25:14:355",2667, 52, 1429, 1751,3836, 770, 5962  ),
-  User("assets/images/2.jpeg", "Johni Holmes", 1, "Day", "12:25:14:355",11847, 9752, 1429, 1751,3836, 350, 99962  ),
-  User("assets/images/3.jpeg", "Alma Simens", 1, "Day", "12:25:14:355",24525, 52, 1429, 1751,3836, 40, 55962  ),
-  User("assets/images/4.jpg", "Henry Graves", 1, "Day", "12:25:14:355",14847, 872, 1429, 1751,3836, 120, 315962  ),
-  User("assets/images/5.jpg", "Johni Holmes", 1, "Day", "12:25:14:355",12847, 32, 1429, 1751,3836, 140, 15962  ),
-  User("assets/images/6.jpg", "Alma Simens", 1, "Day", "12:25:14:355",4847, 45, 1429, 1751,3836, 90, 32962  ),
-  User("assets/images/7.jpg", "Henry Graves", 1, "Day", "12:25:14:355",14847, 54, 1429, 1751,3836, 990, 25962  ),
-];
+// final List<User> users = <User>[
+//   User("assets/images/1.jpeg", "Henry Graves", 1, "Day", "12:25:14:355", 24847,
+//       97, 1429, 1751, 3836, 0, 35962),
+//   User("assets/images/2.jpeg", "Johni Holmes", 1, "Day", "12:25:14:355", 4847,
+//       72, 1429, 1751, 3836, 10, 39962),
+//   User("assets/images/3.jpeg", "Alma Simens", 1, "Day", "12:25:14:355", 14847,
+//       972, 1429, 1751, 3836, 110, 25962),
+//   User("assets/images/4.jpg", "Henry Graves", 1, "Day", "12:25:14:355", 24667,
+//       7, 1429, 1751, 3836, 40, 115962),
+//   User("assets/images/5.jpg", "Johni Holmes", 1, "Day", "12:25:14:355", 847, 9,
+//       1429, 1751, 3836, 540, 55962),
+//   User("assets/images/6.jpg", "Alma Simens", 1, "Day", "12:25:14:355", 5847, 92,
+//       1429, 1751, 3836, 40, 65962),
+//   User("assets/images/7.jpg", "Henry Graves", 1, "Day", "12:25:14:355", 2847, 2,
+//       1429, 1751, 3836, 100, 15962),
+//   User("assets/images/1.jpeg", "Henry Graves", 1, "Day", "12:25:14:355", 2667,
+//       52, 1429, 1751, 3836, 770, 5962),
+//   User("assets/images/2.jpeg", "Johni Holmes", 1, "Day", "12:25:14:355", 11847,
+//       9752, 1429, 1751, 3836, 350, 99962),
+//   User("assets/images/3.jpeg", "Alma Simens", 1, "Day", "12:25:14:355", 24525,
+//       52, 1429, 1751, 3836, 40, 55962),
+//   User("assets/images/4.jpg", "Henry Graves", 1, "Day", "12:25:14:355", 14847,
+//       872, 1429, 1751, 3836, 120, 315962),
+//   User("assets/images/5.jpg", "Johni Holmes", 1, "Day", "12:25:14:355", 12847,
+//       32, 1429, 1751, 3836, 140, 15962),
+//   User("assets/images/6.jpg", "Alma Simens", 1, "Day", "12:25:14:355", 4847, 45,
+//       1429, 1751, 3836, 90, 32962),
+//   User("assets/images/7.jpg", "Henry Graves", 1, "Day", "12:25:14:355", 14847,
+//       54, 1429, 1751, 3836, 990, 25962),
+// ];
---
+++ lib/services/user_api_provider.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:training_project/constants.dart';
+import 'package:training_project/models/user.dart';
+
+//  https://jsonplaceholder.typicode.com/users
+class UserProvider {
+  Future<List<User>> getUser() async {
+    final responce = await http.get(Uri.parse(usersUrl));
+
+    if (responce.statusCode == successCode) {
+      return jsonDecode(responce.body)
+          .map((json) => User.fromJson(json))
+          .toList;
+    } else {
+      throw Exception(loadUsersError);
+    }
+  }
+}
